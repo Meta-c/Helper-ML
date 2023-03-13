2e5385066bb1696c3c49023772ff797a5d621bde
Meta-c <metamedhat116@gmail.com>
2023-03-13 09:41:26 +0200

    first commit

---
+++ ML_functions.py
@@ -0,0 +1,61 @@
+# plot validation and training curves seperately 
+
+def plot_loss_curves(history):
+    loss=history.history["loss"]
+    val_loss=history.history["val_loss"]
+    
+    accuracy=history.history["accuracy"]
+    val_accuracy=history.history["val_accuracy"]
+    
+    epochs= range(len(history.history["loss"]))
+    
+    # Plot loss
+    plt.plot(epochs,loss,label="training_loss")
+    plt.plot(epochs,val_loss,label="val_loss")
+    plt.title("loss")
+    plt.xlabel("epochs")
+    plt.legend()
+    
+    
+     # Plot accuracy
+    plt.figure()
+    plt.plot(epochs,accuracy,label="training_accuracy")
+    plt.plot(epochs,val_accuracy,label="val_accuracy")
+    plt.title("accuracy")
+    plt.xlabel("epochs")
+    plt.legend()
+    
+    
+    
+    
+    
+def view_random_image(target_dir, target_class):
+    # Setup the target directory
+    target_folder=target_dir+target_class
+    
+    # Get a random image path
+    random_image=random.sample(os.listdir(target_folder),1)
+    print(random_image)
+    
+    # Read the image and plot
+    img=mpimg.imread(target_folder+"/"+random_image[0])
+    plt.imshow(img)
+    plt.title(target_class)
+    plt.axis("off")
+    
+    print(f"Image shape: {img.shape}")
+    
+    return img
+
+
+
+def pred_and_plot(model,filename,class_names=class_names):
+    img=load_and_prep_image(filename)
+    
+    pred=model.predict(tf.expand_dims(img,axis=0))
+    
+    pred_class=class_names[int(tf.round(pred))]
+    
+    plt.imshow(img)
+    plt.title(f"Prediction : {pred_class} ")
+    plt.axis(False)
